6ce671ce802d8dc411efe64bd77bb0046d82d06c
khbin501 <khbin501@gmail.com>
2026-08-11 17:49:30 +0900

    t1

--- api/DB.py
+++ api/DB.py
@@ -1,10 +1,20 @@
 import os
+from fastapi import FastAPI
 from upstash_redis import Redis
 from dotenv import load_dotenv
 
+# 1. 비서(dotenv)를 시켜서 .env 파일의 내용을 파이썬으로 불러옵니다.
 load_dotenv()
 
-redis = Redis.from_env()
+app = FastAPI()
 
-redis.set("foo", "bar")
-value = redis.get("foo")
+# 2. 불러온 URL과 TOKEN을 꺼내서 Redis DB에 연결합니다.
+redis = Redis(
+    url=os.getenv("UPSTASH_REDIS_REST_URL"),
+    token=os.getenv("UPSTASH_REDIS_REST_TOKEN")
+)
+
+# 연결 테스트용 API
+@app.get("/")
+def read_root():
+    return {"message": "DB 연결 세팅 완료!"}
