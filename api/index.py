2fd7a3dc8b74141d29bff04311e518866f635a05
khbin501 <khbin501@gamil.com>
2026-08-26 03:22:24 +0000

    set up

--- api/index.py
+++ api/index.py
@@ -1,27 +1,10 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
 from .routers.db import db_router
-import os
-from .routers.schemas import Report
 
 app = FastAPI()
 app.include_router(db_router)
 
-# # 현재 실행 중인 파일(api/index.py)의 부모 폴더(최상위 디렉토리) 경로 계산
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    
-# @app.get("/")
-# def serve_html():
-#     html_path = os.path.join(BASE_DIR, "index.html")
-#     return FileResponse(html_path)
-
-# # [추가] HTML이 자바스크립트 파일을 요구할 때 전달해주기
-# @app.get("/script.js")
-# def serve_js():
-#     js_path = os.path.join(BASE_DIR, "script.js")
-#     return FileResponse(js_path)
-
-@app.get("/api/health_check")
+@app.get("/api/health-check")
 def read_api():
     return {"message": "fastapi server health check."}
 
